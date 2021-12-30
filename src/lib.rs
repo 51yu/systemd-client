08332bd1d00752117915d1f9b918c227e20bb16d
Li Yu <li.yu.sh0211@gmail.com>
2021-12-30 11:22:53 -0800

    Add configuration template for service

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 pub mod errors;
 pub mod manager;
 pub mod models;
+pub mod templates;
 pub mod utils;
 
 pub use errors::*;
 pub use models::*;
+pub use templates::*;
 pub use utils::*;
---
+++ tests/test_templates.rs
@@ -0,0 +1,16 @@
+use systemd_client::{ServiceConfiguration, ServiceUnitConfiguration, UnitConfiguration};
+
+#[test]
+fn test_service_template() {
+    let unit_builder = UnitConfiguration::builder().description("test service");
+    let svc_builder = ServiceConfiguration::builder().exec_start(vec!["/bin/echo", "aloha"]);
+    let svc_unit = ServiceUnitConfiguration::builder()
+        .unit(unit_builder)
+        .service(svc_builder)
+        .build();
+    let actual_svc_unit = format!("{}", svc_unit);
+    let buffer =
+        std::fs::read("tests/resources/test.service").expect("open file 'test.service' failed");
+    let expected_svc_unit = String::from_utf8(buffer).expect("invalid utf-8 in 'test.service'");
+    assert_eq!(expected_svc_unit, actual_svc_unit);
+}
